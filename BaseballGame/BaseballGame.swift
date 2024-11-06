7785cebf6b357242baadcafa89ad33a412ad40d4
Youngjin <bryjna07@gmail.com>
2024-11-06 09:46:57 +0900

    [feat] 디렉토리 분리 및 시작단계

---
+++ BaseballGame/BaseballGame.swift
@@ -0,0 +1,26 @@
+//
+//  main.swift
+//  BaseballGame
+//
+//  Created by YoungJin on 11/4/24.
+//
+
+class BaseballGame {
+    func start() {
+        // 기록 초기화 , 시작멘트,
+            let answer = makeAnswer() // 정답을 만드는 함수
+        }
+        
+        func makeAnswer() -> Int {
+            // 함수 내부를 구현하기 , 서로 다른 임의의 수 3개 생성하기
+            
+            
+            // random , 3개, 3자리, 중복 x
+            
+            return 000
+        }
+    }
+
+// 숫자 입력
+// 결과 판단
+// 예외사항
--- BaseballGame/main.swift
+++ BaseballGame/main.swift
@@ -8,3 +8,7 @@
 // readLine() 함수를 이용하여 유저의 입력값 처리하기
 // readLine() 함수에 대해 학습해보고 활용하기
 let input = readLine()
+
+let game = BaseballGame()
+game.start() // BaseballGame 인스턴스를 만들고 start 함수를 구현하기
+
